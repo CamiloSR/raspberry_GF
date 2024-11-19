a3c687a56d7a27a788270ceb042c1601cb6f688a
Camilo Serna Restrepo <camilosernar@outlook.com>
2024-11-19 14:11:30 -0500

    Update usd_gadget_id_vendor.sh

--- usd_gadget_id_vendor.sh
+++ usd_gadget_id_vendor.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+USB_IMAGE_FILE="/piusb.bin"
+USB_IMAGE_LABEL="PIUSB"
+ID_VENDOR="0x058f"
+ID_PRODUCT="0x6387"
+
+# Create USB image if it doesn't exist
+if [ ! -f "$USB_IMAGE_FILE" ]; then
+    dd if=/dev/zero of="$USB_IMAGE_FILE" bs=1M count=2048
+    mkdosfs -F 32 -n "$USB_IMAGE_LABEL" "$USB_IMAGE_FILE"
+fi
+
 modprobe libcomposite
 cd /sys/kernel/config/usb_gadget/
 mkdir -p g1
 cd g1
 
-echo 0x058f > idVendor    # Replace XXXX with the Vendor ID of the working USB
-echo 0x6387 > idProduct   # Replace YYYY with the Product ID of the working USB
+echo $ID_VENDOR > idVendor
+echo $ID_PRODUCT > idProduct
+echo "0x0100" > bcdDevice
+echo "0x0200" > bcdUSB
 
 mkdir -p strings/0x409
 echo "6&14FAAF72&0&_&0" > strings/0x409/serialnumber
@@ -23,4 +36,5 @@ echo 1 > functions/mass_storage.0/lun.0/removable
 echo "$USB_IMAGE_FILE" > functions/mass_storage.0/lun.0/file
 
 ln -s functions/mass_storage.0 configs/c.1/
-ls /sys/class/udc > UDC
+UDC=$(ls /sys/class/udc | head -n1)
+echo "$UDC" > UDC
