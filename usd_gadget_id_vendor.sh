0263701d0027852e04fd9c23c6c44fb7c6591066
Camilo Serna Restrepo <camilosernar@outlook.com>
2024-11-20 10:28:46 -0500

    Update usd_gadget_id_vendor.sh

--- usd_gadget_id_vendor.sh
+++ usd_gadget_id_vendor.sh
@@ -8,9 +8,9 @@ PID="0x1234"                           # Product ID from working USB
 bcdDevice="0x0100"                     # Device version (1.00)
 bcdUSB="0x0200"                        # USB version (2.0)
 MANUFACTURER="General"                 # Manufacturer string
-PRODUCT="General UDisk USB Device"     # Product string
+PRODUCT="General UDisk USB Device"      # Product string
 SERIALNUMBER="Љ"                       # Serial number from working USB
-USB_IMAGE="/piusb.bin"                 # Path to your USB image
+USB_IMAGE="/piusb.bin"                # Path to your USB image
 
 GADGET_DIR="/sys/kernel/config/usb_gadget/g1"
 
