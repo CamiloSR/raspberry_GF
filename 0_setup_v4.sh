f11eda2b9eb8e43bb1cebf33c15422e4569e36e6
Camilo Serna Restrepo <camilosernar@outlook.com>
2026-01-19 13:58:18 -0500

    Update 0_setup_v4.sh

--- 0_setup_v4.sh
+++ 0_setup_v4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ==============================================================================
-# 0_setup_v3.sh
+# 0_setup_v4.sh
 # Purpose:
 # This script automates system maintenance, configures Raspberry Pi-specific
 # settings (boot behavior and filesystem expansion), installs essential packages
